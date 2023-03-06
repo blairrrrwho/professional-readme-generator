d71eac984f71bf97e87b75686d3ba00fdf1b575b
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-06 16:53:07 -0600

    fixed function in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-const { log } = require('console');
+// const { log } = require('console');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -64,11 +64,6 @@ const questions = [
       },
 ];
 
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(fileName, data);
-}
 
 // TODO: Create a function to initialize app
 function writeToFile(fileName, data) {
