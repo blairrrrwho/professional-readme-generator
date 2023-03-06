058cdc2696c3abd727ea7f2f9464a2e2e9b92f33
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-06 15:44:21 -0600

    added to do .md note; changes to index and package.json

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "1: What is your GitHub username?",
+    "2: What is your email address?",
+    "3: What is your project's name",
+    "4: Write a short description of your project.",
+    "5: What kind of license does your project need?",
+    "6: What command shoud be run to install dependencies?",
+    "7: What command should be run to install tests?",
+    "8: What does the user need to know about using your repo?",
+    "9: What does the user need to know about contributing to this repo?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +24,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
