9bf1442881c04f8740d315d67717bdef89f6af06
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-06 16:31:56 -0600

    changed questions into objects; added properties

--- Develop/index.js
+++ Develop/index.js
@@ -1,28 +1,87 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+const generateMarkdown = require('./utils/generateMarkdown');
+const { log } = require('console');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "1: What is your GitHub username?",
-    "2: What is your email address?",
-    "3: What is your project's name",
-    "4: Write a short description of your project.",
-    "5: What kind of license does your project need?",
-    "6: What command shoud be run to install dependencies?",
-    "7: What command should be run to install tests?",
-    "8: What does the user need to know about using your repo?",
-    "9: What does the user need to know about contributing to this repo?",
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'Email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'input',
+        name: 'Project',
+        message: 'What is your project\'s name?',
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a brief description of your project?',
+      },
+      {
+        type: 'list',
+        name: 'License',
+        message: 'What type of license do you need for your project?',
+        choices: [
+            'MIT',
+            'Apache',
+            'GPLv2',
+            'GPLv3',
+            'BSD 3-clause',
+            'BSD 2-clause',
+            'LGPLv3',
+            'AGPLv3',
+            'Unlicensed',
+        ]
+      },
+      {
+        type: 'input',
+        name: 'Installation',
+        message: 'What command should be ran to install dependencies?',
+      },
+      {
+        type: 'input',
+        name: 'Tests',
+        message: 'What command should be ran to run tests?',
+      },
+      {
+        type: 'input',
+        name: 'User Knowledge',
+        message: 'What does the user need to know in order to use your repo?',
+      },
+      {
+        type: 'input',
+        name: 'Contributing',
+        message: 'What does the user need to know about contributing to this project?',
+      },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data);
+}
+
+const init = (questions) => {
+    inquirer.prompt(questions) .then((response) => {
+        console.log(response);
+        const mdPageContent = generateMarkdown(response);
+        console.log(mdPageContent);
+        writeToFile ('myREADME1.md', mdPageContent)
+    })
+};
 
 // Function call to initialize app
-init();
-
-
+init(questions);
