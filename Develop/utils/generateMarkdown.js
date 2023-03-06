e9a1917e0327e2f4719ffdc0c92b3f7b5de8e1c1
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-06 16:48:12 -0600

    fixed license issue

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(License) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(License) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(License) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -37,7 +37,7 @@ function generateMarkdown(data) {
     ${data.UserKnowledge}
   
     ## License
-    ${data.License}
+    ${renderLicenseSection(data.License)}
   
     ## How to Contribute 
     ${data.Contributing}
