0ff1c1053098292ed2a99e30cdfbb3488c6dc6ce
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-06 16:58:40 -0600

    added if statement in renderLicenseSection function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,7 +18,12 @@ return '';
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(License) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `This project is licensed under the ${license} license.`;
+    }
+return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
