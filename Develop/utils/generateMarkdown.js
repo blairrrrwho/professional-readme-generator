9aaabdc295a9ded1f8f2fc8c512418b52a7c5a61
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-03-06 16:57:22 -0600

    added license badge if statement in renderLicenseBadge function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(License) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![License](https://img.shields.io/badge/license-${license}-green.svg)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
